f50c24beface57b305c2b651138cb1a94fe63957
Hugo Wood <hx4.5@free.fr>
2016-02-08 11:08:26 +0100

    feat: test for tp5

---
+++ tests/step5.test.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const utils = require("./test-utils");
+const expect = utils.expect;
+const express = require("express")
+
+
+describe("app at step 5", function () {
+
+  describe("add", function () {
+
+    it("calls POST /contacts", function (done) {
+      let called = false
+      const server = express().post("/contacts", function (req, res) {
+        called = true
+        res.status(200).end()
+      }).listen(3232, function () {
+        utils.runApp("-- --http add Clark Kent", function (a) {
+          expect(called).to.equal(true);
+          server.close()
+          done();
+        }, err => {throw err});
+      })
+    });
+
+  });
+
+  describe("remove <id>", function () {
+
+    it("calls DELETE /contacts/<id>", function (done) {
+      const id = "id"
+      let called = false
+      express().delete("/contacts/id", function (req, res) {
+        called = true
+        res.status(200).end()
+      }).listen(3232, function () {
+        utils.runApp("-- --http remove id", function () {
+          expect(called).to.equal(true);
+          done();
+        }, err => {throw err});
+      })
+    });
+
+  });
+
+});
