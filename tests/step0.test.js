fb501185108c99ab8aa879b2af583e4ab73b9d2e
Hugo Wood <hx4.5@free.fr>
2017-04-09 18:03:42 +0200

    chore(deps): upgraded mocha, chai, request

--- tests/step0.test.js
+++ tests/step0.test.js
@@ -6,7 +6,7 @@ const expect = require("./test-utils").expect;
 describe("app at step 0", function () {
 
   it("passes this test", function () {
-    expect(true).to.be.true();
+    expect(true).to.be.true;
   });
 
 });
